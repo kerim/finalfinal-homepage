005f02d727b36d35c40c413e8aed29f9ee0195a7
P. Kerim Friedman <59976+kerim@users.noreply.github.com>
2026-02-07 12:18:25 +0530

    specified it is a MacOS app

--- src/config/appInfo.js
+++ src/config/appInfo.js
@@ -1,7 +1,7 @@
 // Main application information
 export const appInfo = {
   title: "FINAL|FINAL",
-  description: "A writing app for academics who need outlining, citations, versioning, and focus — all in one place.",
+  description: "A MacOS writing app for academics who need outlining, citations, versioning, and focus — all in one place.",
   logo: {
     src: "favicon-512.png"
   },
