2b9da442302cf0cb698a644a4828d16d48da079e
P. Kerim Friedman <59976+kerim@users.noreply.github.com>
2026-02-07 13:50:31 +0530

    footer

--- src/config/updates.js
+++ src/config/updates.js
@@ -32,22 +32,6 @@ export const updates = [
       "Improved zoom mode: Option-double-click zooms to a single section, regular double-click shows section and sub-sections",
       "Improved focus mode: full screen, hide side bars, hide header and footer, collapse annotations, and remember state on restart or exit"
     ]
-  },
-  {
-    version: "02.15",
-    changes: [
-      "Complete redesign of backend architecture to better support block-based outlining features",
-      "Cleaned up source-mode so it matches look and theme of WYSIWYG editor",
-      "Improved bibliography generation to make it more robust in various edge cases"
-    ]
-  },
-  {
-    version: "0.2.9",
-    changes: [
-      "Preference pane now lets you override default themes and save your own styles",
-      "Improved Zotero workflow now uses native citation picker and lets you add multiple sources to the same in-text citation",
-      "Improved bibliography sync to better match the text"
-    ]
   }
 ];
 
